2e41d8312d805738c5e5c72c3d4bc38e64549eaf
Alexandr Chepak <djalexbc@gmail.com>
2020-09-22 04:09:02 +0300

    Finish task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const numberOfCategories = document.querySelectorAll('li.item');
+console.log(`В списке ${numberOfCategories.length} категории`);
+
+const itemsCategoryEl = Array.from(document.querySelectorAll('.item'));
+itemsCategoryEl.forEach(value => {
+  const titleOfCategories = value.firstElementChild.textContent;
+  const quantityOfEl = value.querySelectorAll('li').length;
+  console.log(
+    `Категория: ${titleOfCategories} \nКоличество элементов: ${quantityOfEl}`,
+  );
+});
