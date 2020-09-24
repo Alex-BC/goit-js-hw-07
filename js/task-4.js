998f1f2f6b0aaa26abfa53b65335beb228e4244f
Alexandr Chepak <djalexbc@gmail.com>
2020-09-25 01:47:15 +0300

    Finish task-5

---
+++ js/task-4.js
@@ -0,0 +1,23 @@
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+const counter = {
+  counterValue: 0,
+  decrement() {
+    this.counterValue -= 1;
+  },
+  increment() {
+    this.counterValue += 1;
+  },
+};
+
+incrementBtn.addEventListener('click', function () {
+  counter.increment();
+  valueEl.textContent = counter.counterValue;
+});
+
+decrementBtn.addEventListener('click', function () {
+  counter.decrement();
+  valueEl.textContent = counter.counterValue;
+});
