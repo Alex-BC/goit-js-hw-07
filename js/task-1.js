1ddc04f4b07a8bceaf84c5ccba7b73a91312c926
Alexandr Chepak <djalexbc@gmail.com>
2020-09-29 20:46:19 +0300

    Make some addits

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,7 @@
 const numberOfCategories = document.querySelectorAll('li.item');
 console.log(`В списке ${numberOfCategories.length} категории`);
 
-const itemsCategoryEl = Array.from(document.querySelectorAll('.item'));
+const itemsCategoryEl = Array.from(numberOfCategories);
 itemsCategoryEl.forEach(value => {
   const titleOfCategories = value.firstElementChild.textContent;
   const quantityOfEl = value.querySelectorAll('li').length;
--- js/task-6.js
+++ js/task-6.js
@@ -7,11 +7,14 @@ input.addEventListener('input', IsValidNumber);
 function IsValidNumber(event) {
   console.log(event.currentTarget.value.length);
   console.log(validNumberOfEl);
-  if (event.currentTarget.value.length > validNumberOfEl) {
-    input.classList.add('valid');
-    input.classList.remove('invalid');
-  } else if (event.currentTarget.value.length < validNumberOfEl) {
-    input.classList.remove('valid');
+  if (
+    event.currentTarget.value.length < validNumberOfEl ||
+    event.currentTarget.value.length > validNumberOfEl
+  ) {
     input.classList.add('invalid');
+    input.classList.remove('valid');
+  } else {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
   }
 }
